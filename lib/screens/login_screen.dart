e0ea7c1b1e1065901e5e16ce5999a2a0b46aff53
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-12 20:41:57 +0530

    android dependencies

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,72 +24,75 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Instagram",
-              style: TextStyle(
-                fontSize: 50.0,
-                fontFamily: 'Billabong',
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Instagram",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: 'Billabong',
+                ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Email'),
-                      validator: (input) => !input.contains('@')
-                          ? 'Please Enter a valid Email'
-                          : null,
-                      onSaved: (input) => _email = input,
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                        validator: (input) => !input.contains('@')
+                            ? 'Please Enter a valid Email'
+                            : null,
+                        onSaved: (input) => _email = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                      validator: (input) => input.length < 6
-                          ? 'Must be atleast 6 characters'
-                          : null,
-                      onSaved: (input) => _password = input,
-                      obscureText: true,
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input) => input.length < 6
+                            ? 'Must be atleast 6 characters'
+                            : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                    SizedBox(
+                      height: 20.0,
                     ),
-                  ),
-                  SizedBox(height: 20.0,),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: () => Navigator.pushNamed(context, SignupScreen.id),
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Go to SignUp', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20.0,),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: () => Navigator.pushNamed(context, SignupScreen.id),
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Go to SignUp', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -24,83 +24,86 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Instagram",
-              style: TextStyle(
-                fontSize: 50.0,
-                fontFamily: 'Billabong',
+      body: SingleChildScrollView(
+                child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Instagram",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: 'Billabong',
+                ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Name'),
-                      validator: (input) => input.trim().isEmpty
-                          ? 'Please Enter a valid name'
-                          : null,
-                      onSaved: (input) => _name = input,
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Name'),
+                        validator: (input) => input.trim().isEmpty
+                            ? 'Please Enter a valid name'
+                            : null,
+                        onSaved: (input) => _name = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Email'),
-                      validator: (input) => !input.contains('@')
-                          ? 'Please Enter a valid Email'
-                          : null,
-                      onSaved: (input) => _email = input,
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                        validator: (input) => !input.contains('@')
+                            ? 'Please Enter a valid Email'
+                            : null,
+                        onSaved: (input) => _email = input,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                      validator: (input) => input.length < 6
-                          ? 'Must be atleast 6 characters'
-                          : null,
-                      onSaved: (input) => _password = input,
-                      obscureText: true,
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input) => input.length < 6
+                            ? 'Must be atleast 6 characters'
+                            : null,
+                        onSaved: (input) => _password = input,
+                        obscureText: true,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('SignUp', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                    SizedBox(
+                      height: 20.0,
                     ),
-                  ),
-                  SizedBox(height: 20.0,),
-                  Container(
-                    width: 250.0,
-                    child: FlatButton(
-                      onPressed: () => Navigator.pop(context),
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Back to LogIn', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('SignUp', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20.0,),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: () => Navigator.pop(context),
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text('Back to LogIn', style: TextStyle(color: Colors.white, fontSize: 18.0),),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
