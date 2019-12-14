825bf6441841060f4d23623cacbd512ce6a88d0c
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-12-14 13:20:35 +0530

    added new screens for Page View

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/activity_screen.dart';
+import 'package:instagram_clone/screens/create_post_screen.dart';
+
+import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,8 +13,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int _currentTab = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +36,32 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: CupertinoTabBar(
-        currentIndex: _currentTab,
-        onTap: (int index){
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          FeedScreen(),
+          SearchScreen(),
+          CreatePostScreen(),
+          ActivityScreen(),
+          ProfileScreen(),
+        ],
+        onPageChanged: (int index) {
           setState(() {
             _currentTab = index;
           });
+        },
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _currentTab,
+        onTap: (int index) {
+          setState(() {
+            _currentTab = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+          );
         },
         activeColor: Colors.black,
         items: [
